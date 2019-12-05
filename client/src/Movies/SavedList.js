0bc704a9583ee669f40d5bc0aa4e8f795bd1601c
msteele11101 <msteele11101@gmail.com>
2019-12-04 18:46:52 -0800

    MVP and Strech done

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
+      <NavLink to={`/movies/${movie.id}`} activeClassName="selectedLink">
       <span className="saved-movie">{movie.title}</span>
+      </NavLink>
     ))}
-    <div className="home-button">Home</div>
+    
+    <NavLink to={'/'}> <div className="home-button">Home</div></NavLink>
   </div>
 );
 
